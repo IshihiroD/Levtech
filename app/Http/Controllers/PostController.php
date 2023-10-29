85a528a8ad1f0b0508a79f2726162d56435d7eed
IshihiroD <dajj1108daiki@gmail.com>
2023-10-29 21:04:47 +0900

    Add 記事投稿作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,12 @@ class PostController extends Controller
     
     public function show(Post $post)
     {
-        dd($post);#dd関数は変数の中身の確認ができる
+        return view('post/show')->with(['post' => $post]);
+        #dd($post);#dd関数は変数の中身の確認ができる
+    }
+    
+    public function create()
+    {
+        return view('posts/create');
     }
 }
